9fb6f818232b441fc2c5b9542d968cc7b059fb1c
Deepanshu-three <deepanshu.main@gmail.com>
2024-12-02 20:20:56 +0530

    added node card header

--- app/(dashboard)/workflows/_components/nodes/NodeComponent.tsx
+++ app/(dashboard)/workflows/_components/nodes/NodeComponent.tsx
@@ -1,9 +1,16 @@
 import { NodeProps } from "@xyflow/react";
 import { memo } from "react";
 import NodeCard from "./NodeCard";
+import NodeHeader from "./NodeHeader";
+import { AppNodeData } from "@/types/appNode";
 
 const NodeComponent = memo((props: NodeProps) => {
-    return <NodeCard isSelected={!!props.selected} nodeId={props.id}>AppNode</NodeCard>
+    
+    const nodeData = props.data as AppNodeData
+
+    return <NodeCard isSelected={!!props.selected} nodeId={props.id}>
+        <NodeHeader taskType={nodeData.type} /> 
+    </NodeCard>
 })
 
 export default NodeComponent;
--- lib/workflow/task/LaunchBrowser.tsx
+++ lib/workflow/task/LaunchBrowser.tsx
@@ -5,8 +5,8 @@ import { GlobeIcon, LucideProps } from "lucide-react";
 export const LaunchBrowserTask = {
     type: TaskType.LAUNCH_BROWSER,
     label: "Launch Browser",
-    icon: (props: LucideProps) => {
+    icon: (props: LucideProps) => 
         <GlobeIcon className='stroke-pink-400' {...props} />
-    },
+    ,
     isEntryPoint: true
 }   
